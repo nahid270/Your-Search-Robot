a74bfe447a64f2283a2cdcad40c01b6e527b1739
nahid270 <110238003+nahid270@users.noreply.github.com>
2025-11-26 10:20:29 +0600

    Update info.py

--- info.py
+++ info.py
@@ -31,9 +31,9 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 #Making it false will not save caption in db SO you can save some storage space
 
 
-PICS = (environ.get('PICS', 'https://graph.org/file/56b5deb73f3b132e2bb73.jpg https://graph.org/file/5303692652d91d52180c2.jpg https://graph.org/file/425b6f46efc7c6d64105f.jpg https://graph.org/file/876867e761c6c7a29855b.jpg')).split()  # Sample pic
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
+PICS = (environ.get('PICS', 'https://i.ibb.co/PsPHtVLC/photo-2025-11-26-04-14-49-7576882807671095328.jpg')).split()  # Sample pic
+NOR_IMG = environ.get("NOR_IMG", "https://i.ibb.co/prL30T70/photo-2025-11-26-04-16-32-7576883215692988444.jpg")
+MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://i.ibb.co/twrRvhB9/photo-2025-11-26-04-18-01-7576883597945077800.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/242b7f1b52743938d81f1.jpg'))
 FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
